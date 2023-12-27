0c04cc54ae9a59699ab01bb8cae6db31c6a39867
light-planck <seseifunc16@gmail.com>
2023-12-27 14:20:55 +0900

    style: format

--- 03.asynchronous/callback/index.js
+++ 03.asynchronous/callback/index.js
@@ -2,6 +2,7 @@ import sqlite3 from "sqlite3";
 
 const main = () => {
   const db = new sqlite3.Database(":memory:");
+
   db.run(
     "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     () => {
