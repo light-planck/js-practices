5e977155f7477753ec8fd6ac09436a49f0e2138c
light-planck <seseifunc16@gmail.com>
2024-01-10 13:58:34 +0900

    refactor: booksの存在をチェックしない

--- 03.asynchronous/callback/db-operations.js
+++ 03.asynchronous/callback/db-operations.js
@@ -4,7 +4,7 @@ const main = () => {
   const db = new sqlite3.Database(":memory:");
 
   db.run(
-    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     () => {
       db.run("INSERT INTO books (title) VALUES (?)", "Ruby入門", function () {
         console.log(this.lastID);
