7e9612120122ddcdd3b7152e7f199d4decb0a152
light-planck <seseifunc16@gmail.com>
2024-01-10 15:29:35 +0900

    refactor: sqlを文字列として共通化する

--- 03.asynchronous/src/scripts/callback/db-operations.js
+++ 03.asynchronous/src/scripts/callback/db-operations.js
@@ -1,26 +1,29 @@
 import sqlite3 from "sqlite3";
+import {
+  CREATE_BOOKS_TABLE,
+  INSERT_BOOK,
+  SELECT_BOOKS,
+  DROP_BOOKS_TABLE,
+} from "../../sql-queries/index.js";
 
 const main = () => {
   const db = new sqlite3.Database(":memory:");
 
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    () => {
-      db.run("INSERT INTO books (title) VALUES (?)", "Ruby入門", function () {
-        console.log(this.lastID);
+  db.run(CREATE_BOOKS_TABLE, () => {
+    db.run(INSERT_BOOK, "Ruby入門", function () {
+      console.log(this.lastID);
 
-        db.each(
-          "SELECT * FROM books",
-          (_, row) => {
-            console.log(row);
-          },
-          () => {
-            db.run("DROP TABLE books");
-          },
-        );
-      });
-    },
-  );
+      db.each(
+        SELECT_BOOKS,
+        (_, row) => {
+          console.log(row);
+        },
+        () => {
+          db.run(DROP_BOOKS_TABLE);
+        },
+      );
+    });
+  });
 };
 
 main();
--- 03.asynchronous/src/scripts/callback/invalid-db-operations.js
+++ 03.asynchronous/src/scripts/callback/invalid-db-operations.js
@@ -1,26 +1,29 @@
 import sqlite3 from "sqlite3";
+import {
+  CREATE_BOOKS_TABLE,
+  DROP_BOOKS_TABLE,
+  INSERT_BOOK,
+  SELECT_BOOKS_BY_AUTHOR,
+} from "../../sql-queries/index.js";
 
 const main = () => {
   const db = new sqlite3.Database(":memory:");
 
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    () => {
-      db.run("INSERT INTO books (title) VALUES (?)", null, (err) => {
-        console.error(err.message);
+  db.run(CREATE_BOOKS_TABLE, () => {
+    db.run(INSERT_BOOK, null, (err) => {
+      console.error(err.message);
 
-        db.each(
-          "SELECT * FROM books WHERE author = (?)",
-          "steve",
-          () => {},
-          (err) => {
-            if (err) console.error(err.message);
-            db.run("DROP TABLE books");
-          },
-        );
-      });
-    },
-  );
+      db.each(
+        SELECT_BOOKS_BY_AUTHOR,
+        "steve",
+        () => {},
+        (err) => {
+          if (err) console.error(err.message);
+          db.run(DROP_BOOKS_TABLE);
+        },
+      );
+    });
+  });
 };
 
 main();
