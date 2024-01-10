9e6bfa814b2a0e964a5085522d4dc2a2e3ca20f1
light-planck <seseifunc16@gmail.com>
2024-01-10 17:44:51 +0900

    fix: async awaitのプログラムを修正

--- 03.asynchronous/src/scripts/async-await/db-operations.js
+++ 03.asynchronous/src/scripts/async-await/db-operations.js
@@ -1,23 +1,24 @@
 import sqlite3 from "sqlite3";
-import { getAsPromise, runAsPromise } from "../lib/index.js";
+import { runAsPromise, eachAsPromise } from "../../lib/index.js";
+import {
+  CREATE_BOOKS_TABLE,
+  DROP_BOOKS_TABLE,
+  INSERT_BOOK,
+  SELECT_BOOKS,
+} from "../../sql-queries/index.js";
 
 const main = async () => {
   const db = new sqlite3.Database(":memory:");
 
-  await runAsPromise(
-    db,
-    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+  await runAsPromise(db, CREATE_BOOKS_TABLE);
 
-  const id = await runAsPromise(db, "INSERT INTO books (title) VALUES (?)", [
-    "Ruby入門",
-  ]);
-  console.log(id);
+  const result = await runAsPromise(db, INSERT_BOOK, ["Ruby入門"]);
+  console.log(result.lastID);
 
-  const rows = await getAsPromise(db, "SELECT * FROM books");
-  console.log(rows);
+  const rows = await eachAsPromise(db, SELECT_BOOKS);
+  rows.forEach((row) => console.log(row));
 
-  await runAsPromise(db, "DROP TABLE books");
+  await runAsPromise(db, DROP_BOOKS_TABLE);
 };
 
 main();
