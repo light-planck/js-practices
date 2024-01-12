ccfd04b21ef8d8a9403ed6113a78185317a4236c
light-planck <seseifunc16@gmail.com>
2024-01-12 18:59:53 +0900

    refactor: 定数の調整に伴うcallbackスクリプトの調整

--- 03.asynchronous/src/scripts/callback/invalid-db-operations.js
+++ 03.asynchronous/src/scripts/callback/invalid-db-operations.js
@@ -1,25 +1,26 @@
 import sqlite3 from "sqlite3";
 import {
-  CREATE_BOOKS_TABLE,
-  DROP_BOOKS_TABLE,
-  INSERT_BOOK,
-  SELECT_BOOKS_BY_AUTHOR,
-} from "../../sql-queries/index.js";
+  BOOK,
+  CREATE_BOOKS_TABLE_SQL,
+  DROP_BOOKS_TABLE_SQL,
+  INSERT_BOOK_SQL,
+  SELECT_BOOKS_BY_AUTHOR_SQL,
+} from "../../constants/index.js";
 
 const main = () => {
   const db = new sqlite3.Database(":memory:");
 
-  db.run(CREATE_BOOKS_TABLE, () => {
-    db.run(INSERT_BOOK, null, (err) => {
+  db.run(CREATE_BOOKS_TABLE_SQL, () => {
+    db.run(INSERT_BOOK_SQL, null, (err) => {
       console.error(err.message);
 
       db.each(
-        SELECT_BOOKS_BY_AUTHOR,
-        "steve",
+        SELECT_BOOKS_BY_AUTHOR_SQL,
+        BOOK.AUTHOR,
         () => {},
         (err) => {
           if (err) console.error(err.message);
-          db.run(DROP_BOOKS_TABLE);
+          db.run(DROP_BOOKS_TABLE_SQL);
         },
       );
     });
--- 03.asynchronous/src/scripts/callback/valid-db-operations.js
+++ 03.asynchronous/src/scripts/callback/valid-db-operations.js
@@ -1,25 +1,26 @@
 import sqlite3 from "sqlite3";
 import {
-  CREATE_BOOKS_TABLE,
-  INSERT_BOOK,
-  SELECT_BOOKS,
-  DROP_BOOKS_TABLE,
-} from "../../sql-queries/index.js";
+  BOOK,
+  CREATE_BOOKS_TABLE_SQL,
+  DROP_BOOKS_TABLE_SQL,
+  INSERT_BOOK_SQL,
+  SELECT_BOOKS_SQL,
+} from "../../constants/index.js";
 
 const main = () => {
   const db = new sqlite3.Database(":memory:");
 
-  db.run(CREATE_BOOKS_TABLE, () => {
-    db.run(INSERT_BOOK, "Ruby入門", function () {
+  db.run(CREATE_BOOKS_TABLE_SQL, () => {
+    db.run(INSERT_BOOK_SQL, BOOK.TITLE, function () {
       console.log(this.lastID);
 
       db.each(
-        SELECT_BOOKS,
+        SELECT_BOOKS_SQL,
         (_, row) => {
           console.log(row);
         },
         () => {
-          db.run(DROP_BOOKS_TABLE);
+          db.run(DROP_BOOKS_TABLE_SQL);
         },
       );
     });
